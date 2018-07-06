e89cf83ba9c55bd5f139ebe07009a903599fcc8b
Jascha Ehrenreich <jascha@jaeh.at>
2018-07-06 20:02:08 +0200

    simplify state management, update views accordingly

--- includes/templates/magic-gdpr-notice.php
+++ includes/templates/magic-gdpr-notice.php
@@ -10,46 +10,37 @@
 function magic_gdpr_render_notice () {
   $context = Timber::get_context();
 
-  $cookie = isset($_COOKIE[MAGIC_GDPR_COOKIE_SLUG]) ? $_COOKIE[MAGIC_GDPR_COOKIE_SLUG] : false;
-  $config = wp_parse_args($cookie);
-  if ( !empty( $config ) && !empty( $config['configuration_cookies'] ) ) {
+  $cookies = magic_get_option( MAGIC_GDPR_COOKIE_SLUG );
+
+  $cookie_query_string = isset( $_COOKIE[MAGIC_GDPR_COOKIE_SLUG] )
+    ? $_COOKIE[MAGIC_GDPR_COOKIE_SLUG]
+    : 0;
+
+  $config = wp_parse_args( $cookie_query_string );
+
+  if ( !empty( $config ) && !empty( $config['settings'] ) ) {
+    // no need to show this form again.
     return;
   }
-
-  $user = wp_get_current_user();
 
   $context['title'] = magic_get_option( MAGIC_GDPR_SLUG . '_notice_title' );
   $context['content'] = magic_get_option( MAGIC_GDPR_SLUG . '_notice_content' );
   $context['image'] = magic_get_option( MAGIC_GDPR_SLUG . '_notice_image' );
   $context['submit_button_text'] = magic_get_option( MAGIC_GDPR_SLUG . '_submit_button_text', 'Apply Changes' );
-
   $context['details_title'] = magic_get_option( MAGIC_GDPR_SLUG . '_details_title' );
 
-  $default_cookies = 'Configuration Cookies - Remembers your choices and prevents this box from showing up again.\nLogin Cookies - Saves your login until you close the site. Disabling these cookies will prevent you from logging in.';
+  $default_cookies = MAGIC_GDPR_DEFAULT_COOKIES;
 
   $gdpr_cookies = magic_get_option( MAGIC_GDPR_COOKIE_SLUG, $default_cookies );
+  $context['cookies'] = magic_deserialize_cookie( $gdpr_cookies );
 
-  $cookie_strings = explode( PHP_EOL, $gdpr_cookies );
-
-  $context['cookies'] = [];
-
-  foreach ( $cookie_strings as $cookie_string ) {
-    $cookie_array = explode( '-', $cookie_string );
-    $cookie = array(
-      'title' => trim( $cookie_array[0] ),
-      'description' => trim( $cookie_array[1] ),
-      'slug' => MAGIC_GDPR_SLUG . '_' . magic_slugify( $cookie_array[0] ),
-    );
-
-    $cookie['on'] = magic_get_option( $cookie['slug'] . '_enabled', false );
-
-    $context['cookies'][] = $cookie;
-  }
+  $context['accept_all_enabled'] = magic_get_option( MAGIC_GDPR_SLUG . '_accept_all_enabled');
+  $context['accept_all_button_text'] = magic_get_option( MAGIC_GDPR_SLUG . '_accept_all_button_text' );
 
   $context['form'] = array(
     'url' => esc_url( admin_url('admin-post.php') ),
-    'action' => MAGIC_GDPR_SLUG . '_settings',
-    'nonce' => wp_create_nonce( MAGIC_GDPR_SLUG . '_settings' ),
+    'action' => MAGIC_GDPR_COOKIE_SETTINGS_ACTION,
+    'nonce' => wp_create_nonce( MAGIC_GDPR_COOKIE_SETTINGS_ACTION ),
   );
 
   Timber::render( 'views/notice.twig', $context );
