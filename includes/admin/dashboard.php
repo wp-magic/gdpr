6eb0dde91e411939cfc7905d475c9aaed30691b8
Jascha Ehrenreich <jascha@jaeh.at>
2018-07-06 20:01:32 +0200

    update option names and settings

--- includes/admin/dashboard.php
+++ includes/admin/dashboard.php
@@ -5,30 +5,53 @@ add_action( 'admin_menu', function () {
 
   $settings = array(
     array(
-      'name' => 'magic_gdpr_enabled',
+      'name' => MAGIC_GDPR_SLUG . '_enabled',
       'type' => 'checkbox',
       'default' => false,
       'label' => 'Enable GDPR Notice and Cookie blocking',
     ),
     array (
-      'name' => 'magic_gdpr_notice_title',
+      'name' => MAGIC_GDPR_SLUG . '_notice_title',
       'type' => 'text',
       'default' => 'Cookie usage notice',
       'label' => 'Title of the gdpr notice field',
     ),
     array (
-      'name' => 'magic_gdpr_notice_logo',
+      'name' => MAGIC_GDPR_SLUG . '_notice_logo',
       'type' => 'image',
       'label' => 'Logo Image to use next to the header text',
     ),
+
+    array (
+      'name' => MAGIC_GDPR_SLUG . '_submit_buttons_header',
+      'type' => 'header',
+      'value' => 'Submit Buttons',
+      'label' => 'Shown on the bottom of the notice.',
+    ),
+
     array(
-      'name' => 'magic_gdpr_submit_button_text',
+      'name' => MAGIC_GDPR_SLUG . '_submit_button_text',
       'type' => 'text',
       'default' => 'Apply Changes',
       'label' => 'Submit button text',
     ),
+
+    array(
+      'name' => MAGIC_GDPR_SLUG . '_accept_all_enabled',
+      'type' => 'checkbox',
+      'default' => 0,
+      'label' => 'Show the Accept All Button',
+    ),
+
+    array(
+      'name' => MAGIC_GDPR_SLUG . '_accept_all_button_text',
+      'type' => 'text',
+      'default' => 'Accept All Cookies',
+      'label' => 'Accept All Button text',
+    ),
+
     array (
-      'name' => 'magic_gdpr_notice_content',
+      'name' => MAGIC_GDPR_SLUG . '_notice_content',
       'type' => 'wysiwyg',
       'default' => 'Cookies enable us to provide you with a better experience while using this website.',
       'label' => 'Title of the gdpr notice field',
@@ -39,16 +62,25 @@ add_action( 'admin_menu', function () {
     ),
 
     array(
-      'name' => 'magic_gdpr_cookie_header',
+      'name' => MAGIC_GDPR_SLUG . '_cookie_header',
       'type' => 'header',
       'value' => 'cookies',
       'label' => 'Cookie settings. These Cookies will be shown in the notice.',
     ),
+
     array(
       'name' => MAGIC_GDPR_COOKIE_SLUG,
       'type' => 'textarea',
       'default' => MAGIC_GDPR_DEFAULT_COOKIES,
-      'label' => 'Add one cookie per line. Format: NAME ' . MAGIC_GDPR_COOKIE_SEP . ' DESCRIPTION ' . MAGIC_GDPR_COOKIE_SEP . ' cookie_name_1,cookie_name2,... \n<br>Possible cookie group: "auth" - removes all auth cookies',
+      'label' =>
+        'Add one cookie per line. Format: NAME ' .
+        MAGIC_DASHBOARD_COOKIE_SEP .
+        'SLUG' .
+        MAGIC_DASHBOARD_COOKIE_SEP .
+        ' DESCRIPTION ' .
+        MAGIC_DASHBOARD_COOKIE_SEP .
+        ' cookie_name_1,cookie_name2,...' .
+        '<br>Possible cookie group: "auth" - removes all auth cookies',
       'config' => array(
         'textarea_rows' => 5,
         'textarea_cols' => 50,
@@ -81,42 +113,42 @@ add_action( 'admin_menu', function () {
     ),
 
     array(
-      'name' => 'magic_gdpr_style_header',
+      'name' => MAGIC_GDPR_SLUG . '_style_header',
       'type' => 'header',
       'value' => 'Styles',
     ),
     array(
-      'name' => 'magic_gdpr_background_color',
+      'name' => MAGIC_GDPR_SLUG . '_background_color',
       'type' => 'color',
       'default' => '#191919',
       'label' => 'Background color',
     ),
     array(
-      'name' => 'magic_gdpr_title_color',
+      'name' => MAGIC_GDPR_SLUG . '_title_color',
       'type' => 'color',
       'default' => '#ffffff',
       'label' => 'Title color',
     ),
     array(
-      'name' => 'magic_gdpr_text_color',
+      'name' => MAGIC_GDPR_SLUG . '_text_color',
       'type' => 'color',
       'default' => '#ffffff',
       'label' => 'Text color',
     ),
     array(
-      'name' => 'magic_gdpr_border_style',
+      'name' => MAGIC_GDPR_SLUG . '_border_style',
       'type' => 'text',
       'default' => '1px solid',
       'label' => 'Border around GDPR Notice',
     ),
     array(
-      'name' => 'magic_gdpr_border_color',
+      'name' => MAGIC_GDPR_SLUG . '_border_color',
       'type' => 'color',
       'default' => '#ffffff',
       'label' => 'Border color of GDPR Notice',
     ),
     array(
-      'name' => 'magic_gdpr_border_radius',
+      'name' => MAGIC_GDPR_SLUG . '_border_radius',
       'type' => 'text',
       'default' => '1em',
       'label' => 'Border radius of GDPR Notice',
