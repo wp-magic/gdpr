457e9d3175a6ff57c1d76884fdfd1fde0a21c687
Jascha Ehrenreich <jascha@jaeh.at>
2018-07-09 17:18:38 +0200

    use string instead of lambda

--- includes/post/gdpr-settings.php
+++ includes/post/gdpr-settings.php
@@ -1,9 +1,5 @@
 <?php
 
-add_action( 'admin_post_nopriv_' . MAGIC_GDPR_COOKIE_SETTINGS_ACTION, function () {
-  magic_gdpr_set_cookies();
-} );
+add_action( 'admin_post_nopriv_' . MAGIC_GDPR_COOKIE_SETTINGS_ACTION, 'magic_gdpr_set_cookies' );
 
-add_action( 'admin_post_' . MAGIC_GDPR_COOKIE_SETTINGS_ACTION, function() {
-  magic_gdpr_set_cookies();
-} );
+add_action( 'admin_post_' . MAGIC_GDPR_COOKIE_SETTINGS_ACTION, 'magic_gdpr_set_cookies' );
