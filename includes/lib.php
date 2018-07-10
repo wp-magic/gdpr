8b1f41175b24e12f7fdfc2a6dd8529f0247459ff
Jascha Ehrenreich <jascha@jaeh.at>
2018-07-10 18:23:11 +0200

    clean

--- includes/lib.php
+++ includes/lib.php
@@ -2,24 +2,9 @@
 
 if ( !function_exists( 'magic_gdpr_check_cookies') ) {
   function magic_gdpr_check_cookies() {
-    $cookies = wp_parse_args( $_COOKIE[MAGIC_GDPR_COOKIE_SLUG] );
-
-    if ( !empty( $_POST['allow_cookies'] ) ) {
-      magic_add_query_arg( ['allow_cookies' => 1] );
-    }
-
-    if ( empty( $cookies['auth'] ) ) {
-      if ( !empty( $_POST['allow_cookies'] ) ) {
-        magic_gdpr_set_cookies( array('settings', 'auth') );
-      } else {
-        magic_add_query_error( 'cookie' );
-      }
-    }
-
-    magic_redirect_if_error();
+    return !empty( $_COOKIE[MAGIC_GDPR_COOKIE_SLUG] );
   }
 }
-
 
 if ( !function_exists( 'magic_gdpr_remove_cookies' ) ) {
   function magic_gdpr_remove_cookies() {
@@ -46,8 +31,6 @@ if ( !function_exists( 'magic_gdpr_remove_cookies' ) ) {
 
 if ( !function_exists( 'magic_gdpr_set_cookies' ) ) {
   function magic_gdpr_set_cookies( array $forced_cookies = [] ) {
-    $ref = $_SERVER['HTTP_REFERER'];
-
     $gdpr_cookies = magic_get_option( MAGIC_GDPR_COOKIE_SLUG, MAGIC_GDPR_DEFAULT_COOKIES );
 
     $cookies = magic_deserialize_cookie($gdpr_cookies);
@@ -58,6 +41,7 @@ if ( !function_exists( 'magic_gdpr_set_cookies' ) ) {
       $allowed_cookies = wp_parse_args( $_COOKIE[MAGIC_GDPR_COOKIE_SLUG] );
 
       $slug = $cookie['slug'];
+
       if ( !empty( $cookie['on'] ) || in_array( $slug, $forced_cookies ) ) {
         $cookie_query_string = add_query_arg( $slug, 1, $cookie_query_string );
       }
@@ -66,13 +50,12 @@ if ( !function_exists( 'magic_gdpr_set_cookies' ) ) {
     $cookie_query_string = substr( $cookie_query_string, 1 );
 
     if ( empty( $cookie_query_string ) ) {
-      wp_redirect( $ref );
-      exit;
+      return;
     }
 
     $one_year = 365 * 60 * 60 * 24;
     $path = '/';
-    $domain = ($_SERVER['HTTP_HOST'] != 'localhost') ? $_SERVER['HTTP_HOST'] : false;
+    $domain = ($_SERVER['HTTP_HOST'] !== 'localhost') ? $_SERVER['HTTP_HOST'] : false;
     $secure = isset( $_SERVER['HTTPS'] );
     $http_only = true;
 
@@ -85,8 +68,5 @@ if ( !function_exists( 'magic_gdpr_set_cookies' ) ) {
       $secure,
       $http_only
     );
-
-    wp_redirect( $ref );
-    exit;
   }
 }
