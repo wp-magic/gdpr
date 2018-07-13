2881f7e1d9c8b110aa906cb04b2f5e76b6671cec
Jascha Ehrenreich <jascha@jaeh.at>
2018-07-13 16:10:35 +0200

    add globals

--- magic-gdpr.php
+++ magic-gdpr.php
@@ -23,6 +23,10 @@ if ( ! defined( 'WPINC' ) ) {
   die;
 }
 
+if ( !defined( 'MAGIC_DASHBOARD_COOKIE_SEP' ) ) {
+  define( 'MAGIC_DASHBOARD_COOKIE_SEP', '|||' );
+}
+
 define( 'MAGIC_GDPR_SLUG', 'magic_gdpr' );
 define( 'MAGIC_GDPR_COOKIE_SLUG', 'magic_gdpr_cookie' );
 define( 'MAGIC_GDPR_COOKIE_SETTINGS_SLUG', 'magic_gdpr_cookies' );
@@ -44,6 +48,33 @@ define( 'MAGIC_GDPR_DEFAULT_COOKIES',
   MAGIC_DASHBOARD_COOKIE_SEP .
   'auth'
 );
+
+define( 'MAGIC_GDPR_CUSTOM_FIELDS', array(
+  'tab_cookies' => array(
+    'type' => 'tab',
+    'label' => 'GDPR settings',
+  ),
+
+  'before_allow_cookies_text' => array (
+    'label' => 'Text before Allow Login Cookie Checkbox',
+    'type' => 'wysiwyg',
+    'default_value' => 'Cookies are small pieces of data saved on your computer.
+
+Login Cookies are used to save your session and allow you to stay logged in to our site.
+
+If you do not allow login cookies, the login will not work.',
+  ),
+
+  'allow_cookies_label' => array (
+    'label' => 'Text shown next to cookie checkbox',
+    'default_value' => 'Accept login cookies',
+  ),
+
+  'after_allow_cookies_text' => array (
+    'label' => 'Text after Allow Login Cookie Checkbox',
+    'type' => 'wysiwyg',
+  ),
+) );
 
 $path = str_replace(ABSPATH, '', plugin_dir_path( __FILE__ ) );
 define( 'MAGIC_GDPR_FORM_INPUT_TEMPLATE', 'magic-gdpr-form-inputs.twig' );
